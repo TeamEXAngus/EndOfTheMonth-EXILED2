72a823ef87b4c59473c3a87ee3ac63f2fc7dd652
Will Morris <teamexangus@gmail.com>
2021-06-12 19:16:08 +1000

    Added config descriptions

--- EndOfTheMonth/Config.cs
+++ EndOfTheMonth/Config.cs
@@ -1,12 +1,20 @@
 ﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
 
 namespace EndOfTheMonth
 {
     public sealed class Config : IConfig
     {
+        [Description("Whether or not this plugin is enabled.")]
         public bool IsEnabled { get; set; } = true;
+
+        [Description("The percent chance that a round will be an event round.")]
         public int PercentChance { get; set; } = 10;
+
+        [Description("Whether or not the event should happen in the Class D cells.")]
         public bool EventInClassDCells { get; set; } = true;
+
+        [Description("Whether or not the event should happen in the Evac Shelter.")]
         public bool EventInEvacShelter { get; set; } = true;
     }
 }
